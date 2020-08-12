384a2e1f295599b4a294f9e41c4ea57f397e57a8
Dominik Pethö <dominikpetho@gmail.com>
2020-08-12 09:01:29 +0200

    fix: Rename tableProvider to collectionProvider

--- Sample/View Controllers/Collection View/CollectionProviderSampleViewController.swift
+++ Sample/View Controllers/Collection View/CollectionProviderSampleViewController.swift
@@ -34,7 +34,7 @@ final class CollectionProviderViewSampleController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    fileprivate lazy var tableProvider = GRCollectionViewProvider<Section>()
+    fileprivate lazy var collectionProvider = GRCollectionViewProvider<Section>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,29 +45,28 @@ final class CollectionProviderViewSampleController: UIViewController {
     }
     
     private func setupTableView() {
-        tableProvider.configureCellSize = { _, cv, index, item in
+        collectionProvider.configureCellSize = { _, cv, index, item in
             return CGSize(width: (cv.frame.width - 21) / 3, height: (cv.frame.width - 21) / 3)
         }
         
-        tableProvider.sectionInsets = .init(top: 0, left: 5, bottom: 0, right: 5)
-        tableProvider.minimumLineSpacingForSection = 5
-        tableProvider.minInteritemSpacingForSection = 5
+        collectionProvider.sectionInsets = .init(top: 0, left: 5, bottom: 0, right: 5)
+        collectionProvider.minimumLineSpacingForSection = 5
+        collectionProvider.minInteritemSpacingForSection = 5
         
-        tableProvider.configureSupplementaryElementOfKind = { provider, cv, index, type in
-            debugPrint(type)
+        collectionProvider.configureSupplementaryElementOfKind = { provider, cv, index, type in
             let section = provider.sections[index.section]
             let view = cv.dequeueReusableSupplementaryView(ofKind: type, fromClass: SimpleCollectionViewSupplementaryView.self, for: index)
             view.titleLabel.text = section.title
             return view
         }
         
-        tableProvider.configureCell = { _, tv, indexPath, item in
+        collectionProvider.configureCell = { _, tv, indexPath, item in
             let cell = tv.dequeueReusableCell(fromClass: SimpleCollectionViewCell.self, for: indexPath)
             cell.titleLabel.text = item.title
             return cell
         }
         
-        tableProvider.configureOnItemSelected = { [unowned self] _, _, _, item in
+        collectionProvider.configureOnItemSelected = { [unowned self] _, _, _, item in
             let alert = UIAlertController(title: "Wow!", message: "You clicked an item: \(item)", preferredStyle: .alert)
             alert.addAction(.init(title: "Cancel", style: .cancel, handler: nil))
             self.present(alert, animated: true)
@@ -77,7 +76,7 @@ final class CollectionProviderViewSampleController: UIViewController {
     private func showItems() {
         let section1 = Section(items: (1...5).map { Section.Item(title: "Item \($0)") }, title: "Section 1")
         let section2 = Section(items: (10...12).map { Section.Item(title: "Item \($0)") }, title: "Section 2")
-        tableProvider.bind(to: collectionView, sections: [section1, section2])
+        collectionProvider.bind(to: collectionView, sections: [section1, section2])
     }
     
 }
