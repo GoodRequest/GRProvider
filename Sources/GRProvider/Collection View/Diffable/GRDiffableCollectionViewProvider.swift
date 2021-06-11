f9e779b1d8d5b1fa48f4bacd43ae70c6850603e3
andrej.jasso@goodrequest.com <andrejjasso>
2021-06-11 15:41:09 +0200

    feat: Added Collection view should select and deselect configuration options. Refactored both files

--- Sources/GRProvider/Collection View/Diffable/GRDiffableCollectionViewProvider.swift
+++ Sources/GRProvider/Collection View/Diffable/GRDiffableCollectionViewProvider.swift
@@ -9,116 +9,162 @@
 import UIKit
 
 @available(iOS 13.0, *)
-open class GRDiffableCollectionViewProvider<Section: Sectionable>: NSObject, UICollectionViewDelegate where Section: Hashable, Section.Item: Hashable {
-    
+open class GRDiffableCollectionViewProvider<Section: Sectionable>:
+    NSObject, UICollectionViewDelegate where Section: Hashable, Section.Item: Hashable {
+
     public typealias DataSource = UICollectionViewDiffableDataSource<Section, Section.Item>
     public typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Section.Item>
-    
+
     public typealias ItemProvider = (GRDiffableCollectionViewProvider, UICollectionView, IndexPath, Section.Item) -> UICollectionViewCell
     public typealias SupplementaryViewProvider = (GRDiffableCollectionViewProvider, UICollectionView, IndexPath, String) -> UICollectionReusableView
     public typealias SwipeGestureProvider = (GRDiffableCollectionViewProvider, UICollectionView, IndexPath, Section.Item) -> ([UIContextualAction])
     public typealias ItemSelectionProvider = (GRDiffableCollectionViewProvider, UICollectionView, IndexPath, Section.Item) -> ()
-    
+
     public typealias ScrollProvider = (UIScrollView) -> ()
     public typealias DidEndDraggingProvider = (UIScrollView, Bool) -> ()
     public typealias WillEndDraggingProvider = (UIScrollView, CGPoint, UnsafeMutablePointer<CGPoint>) -> ()
-    
+    public typealias ItemSelectionStateApprovalProvider = (GRDiffableCollectionViewProvider<Section>, UICollectionView, IndexPath, Section) -> (Bool)
+
     public init(collectionView: UICollectionView) {
         super.init()
-        
+
         dataSource = DataSource(collectionView: collectionView) { [unowned self] (collectionView, indexPath, item) -> UICollectionViewCell? in
             return self.configureCell?(self, collectionView, indexPath, item)
         }
-        
+
         dataSource?.supplementaryViewProvider = { [unowned self] (collectionView, string, indexPath) -> UICollectionReusableView? in
             return self.configureSupplementaryElementOfKind?(self, collectionView, indexPath, string)
         }
     }
-    
+
     ///Data source
     private(set) var dataSource: DataSource?
-    
+
     ///Array of sections
     public var sections: [Section] {
         return dataSource?.snapshot().sectionIdentifiers ?? []
     }
-    
+
     ///Configure cells based on the section item.
     open var configureCell: ItemProvider? = nil
-    
+
     ///Returns Suplementary view for section
     open var configureSupplementaryElementOfKind: SupplementaryViewProvider? = nil
 
     ///Configure sections insets based on the cell section item
     open var configureDidScroll: ScrollProvider? = nil
-    
+
     ///Configure on item selection action based on the selected item
     open var configureOnItemSelected: ItemSelectionProvider? = nil
-        
+
     /// Closure calls when user scrolls in table view. It's closure of UIScrollViewDelegate method `scrollViewDidScroll`
     open var configureRefreshGesture: ScrollProvider? = nil
-    
+
     /// Closure calls when `scrollViewDidEndDragging` fires up.
     open var configureDidEndDragging: DidEndDraggingProvider? = nil
-    
+
     /// Closure calls when `scrollViewDidEndDragging` fires up.
     open var configureWillEndDragging: WillEndDraggingProvider? = nil
-    
+
+    /// Closure calls when `collectionView should deselect same item in multiselect mode` fires up.
+    open var configureShouldDeselectItem: ItemSelectionStateApprovalProvider? = nil
+
+    /// Closure calls when `collectionView should select item` fires up.
+    open var configureShouldSelectItem: ItemSelectionStateApprovalProvider? = nil
+
     public var count: Int {
         return sections.map { $0.items.count }.reduce(0, +)
     }
-        
-    open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    // MARK: - Collection View Delegate
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        didSelectItemAt indexPath: IndexPath
+    ) {
         let selectedItem = sections[indexPath]
         configureOnItemSelected?(self, collectionView, indexPath, selectedItem)
     }
-    
-    open func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        shouldDeselectItemAt indexPath: IndexPath
+    ) -> Bool {
+        let section = sections[indexPath.section]
+        return configureShouldDeselectItem?(self, collectionView, indexPath, section) ?? true
+    }
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        shouldSelectItemAt indexPath: IndexPath
+    ) -> Bool {
+        let section = sections[indexPath.section]
+        return configureShouldSelectItem?(self, collectionView, indexPath, section) ?? true
+    }
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        willDisplay cell: UICollectionViewCell,
+        forItemAt indexPath: IndexPath
+    ) {
         if let cell = cell as? Displayable {
             cell.willDisplayCell()
         }
     }
-    
-    open func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        didEndDisplaying cell: UICollectionViewCell,
+        forItemAt indexPath: IndexPath
+    ) {
         if let cell = cell as? Displayable {
             cell.didEndDisplayCell()
         }
     }
-        
+
+    // MARK: - Scroll View delegate
+
     open func scrollViewDidScroll(_ scrollView: UIScrollView) {
         configureDidScroll?(scrollView)
     }
-    
-    // MARK: - Scroll View delegate
 
-    open func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    open func scrollViewDidEndDragging(
+        _ scrollView: UIScrollView,
+        willDecelerate decelerate: Bool
+    ) {
         configureDidEndDragging?(scrollView, decelerate)
-        
+
         if scrollView.refreshControl?.isRefreshing ?? false {
             configureRefreshGesture?(scrollView)
         }
     }
-    
-    open func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+
+    open func scrollViewWillEndDragging(
+        _ scrollView: UIScrollView,
+        withVelocity velocity: CGPoint,
+        targetContentOffset: UnsafeMutablePointer<CGPoint>
+    ) {
         configureWillEndDragging?(scrollView, velocity, targetContentOffset)
     }
-    
+
     // MARK: - Binding
-    
+
     /// Binds items to collection view with default reload animation
-    public func bind(to collectionView: UICollectionView,
-                     sections: [Section],
-                     animated: Bool = true,
-                     onComplete: @escaping () -> () = {}) {
+    public func bind(
+        to collectionView: UICollectionView,
+        sections: [Section],
+        animated: Bool = true,
+        onComplete: @escaping () -> () = {}
+    ) {
         var snapshot = Snapshot()
-                
+
         collectionView.delegate = self
-        
+
         snapshot.appendSections(sections)
         for section in sections {
             snapshot.appendItems(section.items, toSection: section)
         }
-        
+
         dataSource?.apply(snapshot, animatingDifferences: animated, completion: onComplete)
     }
 
--- Sources/GRProvider/Collection View/GRCollectionViewProvider.swift
+++ Sources/GRProvider/Collection View/GRCollectionViewProvider.swift
@@ -9,8 +9,9 @@
 import DeepDiff
 import UIKit
 
-open class GRCollectionViewProvider<Section: Sectionable>: NSObject, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    
+open class GRCollectionViewProvider<Section: Sectionable>:
+    NSObject, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
     public typealias ItemProvider = (GRCollectionViewProvider, UICollectionView, IndexPath, Section.Item) -> UICollectionViewCell
     public typealias ItemSizeProvider = (GRCollectionViewProvider, UICollectionView, IndexPath, Section.Item) -> CGSize
     public typealias SupplementaryViewProvider = (GRCollectionViewProvider, UICollectionView, IndexPath, String) -> UICollectionReusableView
@@ -18,22 +19,23 @@ open class GRCollectionViewProvider<Section: Sectionable>: NSObject, UICollectio
     public typealias SectionInsetProvider = (GRCollectionViewProvider, UICollectionView, Section) -> UIEdgeInsets
     public typealias ItemSelectionProvider = (GRCollectionViewProvider, UICollectionView, IndexPath, Section.Item) -> ()
     public typealias MinLineSpacingProvider = (GRCollectionViewProvider, UICollectionView, Section) -> CGFloat
-    
+    public typealias ItemSelectionStateApprovalProvider = (GRDiffableCollectionViewProvider<Section>, UICollectionView, IndexPath, Section) -> (Bool)
+
     public typealias ScrollProvider = (UIScrollView) -> ()
     public typealias DidEndDraggingProvider = (UIScrollView, Bool) -> ()
     public typealias WillEndDraggingProvider = (UIScrollView, CGPoint, UnsafeMutablePointer<CGPoint>) -> ()
-    
+
     weak var collectionView: UICollectionView!
-    
+
     /// Array of sections
     internal (set) public var sections: [Section] = []
-    
+
     /// Configure cells based on the section item.
     open var configureCell: ItemProvider? = nil
-    
+
     /// Returns Suplementary view for section
     open var configureSupplementaryElementOfKind: SupplementaryViewProvider? = nil
-    
+
     /// Configure item size based on the cell section item
     open var configureCellSize: ItemSizeProvider? = nil {
         didSet {
@@ -41,7 +43,7 @@ open class GRCollectionViewProvider<Section: Sectionable>: NSObject, UICollectio
             assertionFailure("⚠️ *cellSize* property is already initialized. Property *itemSize* will be ignored and this closure will be used.")
         }
     }
-    
+
     /// Configure sections insets based on the cell section item.
     open var configureSectionInsets: SectionInsetProvider? = nil {
         didSet {
@@ -49,7 +51,7 @@ open class GRCollectionViewProvider<Section: Sectionable>: NSObject, UICollectio
             assertionFailure("⚠️ *sectionInsets* property already initialized. Property *sectionInsets* will be ignored.")
         }
     }
-    
+
     /// Configure min spacing for sections on the cell section item.
     open var configureMinLineSpacingForSection: MinLineSpacingProvider? = nil {
         didSet {
@@ -57,7 +59,7 @@ open class GRCollectionViewProvider<Section: Sectionable>: NSObject, UICollectio
             assertionFailure("⚠️ *sectionInsets* property already initialized. Property *sectionInsets* will be ignored.")
         }
     }
-    
+
     /// Configure min spacing for sections on the cell section item.
     open var configureMinInteritemSpacingForSection: MinLineSpacingProvider? = nil {
         didSet {
@@ -70,16 +72,22 @@ open class GRCollectionViewProvider<Section: Sectionable>: NSObject, UICollectio
     open var configureDidScroll: ScrollProvider? = nil
     /// Configure on item selection action based on the selected item
     open var configureOnItemSelected: ItemSelectionProvider? = nil
-        
+
     /// Closure calls when user scrolls in table view. It's closure of UIScrollViewDelegate method `scrollViewDidScroll`
     open var configureRefreshGesture: ScrollProvider? = nil
-    
+
     /// Closure calls when `scrollViewDidEndDragging` fires up.
     open var configureDidEndDragging: DidEndDraggingProvider? = nil
-    
+
     /// Closure calls when `scrollViewDidEndDragging` fires up.
     open var configureWillEndDragging: WillEndDraggingProvider? = nil
-    
+
+    /// Closure calls when `collectionView should deselect same item in multiselect mode` fires up.
+    open var configureShouldDeselectItem: ItemSelectionStateApprovalProvider? = nil
+
+    /// Closure calls when `collectionView should select item` fires up.
+    open var configureShouldSelectItem: ItemSelectionStateApprovalProvider? = nil
+
     /// Variable representing item size for the row.
     open var cellSize: CGSize? {
         didSet {
@@ -87,114 +95,196 @@ open class GRCollectionViewProvider<Section: Sectionable>: NSObject, UICollectio
             assertionFailure("⚠️ *configureCell* is already assigned.")
         }
     }
-    
+
     /// Variable representing sections instets for whole collection view
     open var sectionInsets: UIEdgeInsets? {
         didSet {
-             guard configureSectionInsets != nil else { return }
+            guard configureSectionInsets != nil else { return }
             assertionFailure("⚠️ *configureSectionInsets* is already assigned. this property will be ignored.")
         }
     }
-    
+
     /// Variable minimum line spacing of sections for whole collection view
     open var minimumLineSpacingForSection: CGFloat?
     {
         didSet {
-             guard configureMinLineSpacingForSection != nil else { return }
+            guard configureMinLineSpacingForSection != nil else { return }
             assertionFailure("⚠️ *configureMinLineSpacingForSection* is already assigned. this property will be ignored.")
         }
     }
-    
+
     /// Variable minimum line spacing of sections for whole collection view
     open var minInteritemSpacingForSection: CGFloat?
     {
         didSet {
-             guard configureMinInteritemSpacingForSection != nil else { return }
+            guard configureMinInteritemSpacingForSection != nil else { return }
             assertionFailure("⚠️ *configureMinInteritemSpacingForSection* is already assigned. this property will be ignored.")
         }
     }
-    
+
     public var count: Int {
         return sections.map { $0.items.count }.reduce(0, +)
     }
-    
-    open func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+    // MARK: - DataSource
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
         return sections[section].items.count
     }
-    
+
     open func numberOfSections(in collectionView: UICollectionView) -> Int {
         sections.count
     }
-    
-    open func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
         return configureCell?(self, collectionView, indexPath, sections[indexPath]) ?? UICollectionViewCell()
     }
-        
-    open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        viewForSupplementaryElementOfKind kind: String,
+        at indexPath: IndexPath
+    ) -> UICollectionReusableView {
+        return configureSupplementaryElementOfKind?(self, collectionView, indexPath, kind) ?? UICollectionReusableView(frame: .zero)
+    }
+
+    // MARK: - Collection View Delegate
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        didSelectItemAt indexPath: IndexPath
+    ) {
         let selectedItem = sections[indexPath]
         configureOnItemSelected?(self, collectionView, indexPath, selectedItem)
     }
-    
-    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return configureSectionInsets?(self, collectionView, sections[section]) ?? sectionInsets ?? UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        shouldDeselectItemAt indexPath: IndexPath
+    ) -> Bool {
+        let section = sections[indexPath.section]
+        return configureShouldDeselectItem?(self, collectionView, indexPath, section) ?? true
     }
-        
-    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        return configureCellSize?(self, collectionView, indexPath, sections[indexPath]) ?? cellSize ?? .zero
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        shouldSelectItemAt indexPath: IndexPath
+    ) -> Bool {
+        let section = sections[indexPath.section]
+        return configureShouldSelectItem?(self, collectionView, indexPath, section) ?? true
     }
-    
-    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return configureMinLineSpacingForSection?(self, collectionView, sections[section]) ?? minimumLineSpacingForSection ?? 0
-    }
-    
-    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return configureMinInteritemSpacingForSection?(self, collectionView, sections[section]) ?? minInteritemSpacingForSection ?? 0
-    }
-    
-    open func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        willDisplay cell: UICollectionViewCell,
+        forItemAt indexPath: IndexPath
+    ) {
         if let cell = cell as? Displayable {
             cell.willDisplayCell()
         }
     }
-    
-    open func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        didEndDisplaying cell: UICollectionViewCell,
+        forItemAt indexPath: IndexPath
+    ) {
         if let cell = cell as? Displayable {
             cell.didEndDisplayCell()
         }
     }
-    
-    open func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        return configureSupplementaryElementOfKind?(self, collectionView, indexPath, kind) ?? UICollectionReusableView(frame: .zero)
+
+    // MARK: - Layout
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAt section: Int
+    ) -> UIEdgeInsets {
+        return configureSectionInsets?(
+            self,
+            collectionView,
+            sections[section]) ?? sectionInsets ?? UIEdgeInsets.zero
     }
-        
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return configureCellSize?(
+            self,
+            collectionView,
+            indexPath, sections[indexPath]) ?? cellSize ?? .zero
+    }
+
+    open func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumLineSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        return configureMinLineSpacingForSection?(
+            self,
+            collectionView,
+            sections[section]) ?? minimumLineSpacingForSection ?? 0
+    }
+
+    open func collectionView(
+        _ collectionView: UICollectionView, l
+            ayout collectionViewLayout: UICollectionViewLayout,
+        minimumInteritemSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        return configureMinInteritemSpacingForSection?(
+            self,
+            collectionView,
+            sections[section]) ?? minInteritemSpacingForSection ?? 0
+    }
+
     open func scrollViewDidScroll(_ scrollView: UIScrollView) {
         configureDidScroll?(scrollView)
     }
-    
+
     // MARK: - Scroll View delegate
 
-    open func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    open func scrollViewDidEndDragging(
+        _ scrollView: UIScrollView,
+        willDecelerate decelerate: Bool
+    ) {
         configureDidEndDragging?(scrollView, decelerate)
-        
+
         if scrollView.refreshControl?.isRefreshing ?? false {
             configureRefreshGesture?(scrollView)
         }
     }
-    
-    open func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+
+    open func scrollViewWillEndDragging(
+        _ scrollView: UIScrollView,
+        withVelocity velocity: CGPoint,
+        targetContentOffset: UnsafeMutablePointer<CGPoint>
+    ) {
         configureWillEndDragging?(scrollView, velocity, targetContentOffset)
     }
-    
+
     // MARK: - Binding
-    
+
     /// Binds items to collection view with default reload animation
-    public func bind(to collectionView: UICollectionView, sections: [Section], animated: Bool = true, onComplete: @escaping () -> () = {}) {
+    public func bind(
+        to collectionView: UICollectionView,
+        sections: [Section],
+        animated: Bool = true,
+        onComplete: @escaping () -> () = {}
+    ) {
         self.collectionView = collectionView
         self.sections = sections
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
-        
+
         collectionView.reloadData(animated: animated) {
             onComplete()
         }
